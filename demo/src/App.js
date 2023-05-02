d8d6c56e3b21698d33fe46d2f881c8f95d61b36f
FarrahYasin <farah.y.yassin@gmail.com>
2023-05-02 09:57:24 +0000

    class16_react1

---
+++ demo/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+import Home from "./components/home/Home";
+import dataf2 from "../src/data/db.json";
+
+
+function App() {
+  return (
+<Home dataJson2 = {dataf2}/> 
+
+    );
+}
+
+export default App;
---
+++ demo/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer(props) {
+    return(
+<div>
+    <a href='www.facebooke.com'>FaceBook</a>
+    <a href='www.facebooke.com'>FaceBook</a>
+    <a href='www.facebooke.com'>FaceBook</a>
+    <p>Copyright saved for Farah Yasin</p>
+
+</div>
+
+    )
+}
