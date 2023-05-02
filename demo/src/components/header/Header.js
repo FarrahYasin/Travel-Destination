f7c5ef7722aa77e8ae32f11f9e2a974953ce09af
FarrahYasin <farah.y.yassin@gmail.com>
2023-05-02 18:52:39 +0000

    try1_class17

--- demo/src/components/header/Header.js
+++ demo/src/components/header/Header.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
 export default function Header(props) {
     return(
     <div>
         <h1>Travells</h1>
+        <Link to = {"/"}>Home</Link>
+
     </div>
+
 
     )
 }
--- demo/src/components/tours/Tours.js
+++ demo/src/components/tours/Tours.js
@@ -1,17 +1,17 @@
+
+import Tour from "../tours/tour/Tour";
+
 export default function Tours(props) {
     return(
-<div>
-{props.d1.map(item =>{
-return(
-<div>
-<p>{item.name}</p>
-<img src = {item.image} alt={item.name}/>
-</div>
-)
-}
-)
-}
-</div>
-
+        <div>
+     {props.d1.map(item =>{
+    
+    return ( 
+        <div key = {item.id}>
+      <Tour name={item.name} image ={item.image} id = {item.id}/>
+      </div>
     )
-}
+    
+    })}
+    </div>
+    )};
